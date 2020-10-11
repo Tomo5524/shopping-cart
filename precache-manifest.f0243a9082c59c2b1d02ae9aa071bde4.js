66bf1da975a7da620483f393c1edd95d452d260b
Tomo5524 <ttoommoosrst@gmail.com>
2020-10-11 16:51:45 +0900

    Updates

---
+++ precache-manifest.f0243a9082c59c2b1d02ae9aa071bde4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1225265cdc97b3d8b7e416782459c3f",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "492f0b6a9c51efbaa7e3",
+    "url": "/shopping-cart/static/css/main.e3f84fed.chunk.css"
+  },
+  {
+    "revision": "f4c2db58cbabc9307833",
+    "url": "/shopping-cart/static/js/2.2c0271fb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopping-cart/static/js/2.2c0271fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "492f0b6a9c51efbaa7e3",
+    "url": "/shopping-cart/static/js/main.484c501e.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "bafc1dc4937f7ecf7cc707d32500b60d",
+    "url": "/shopping-cart/static/media/Mars.bafc1dc4.jpg"
+  },
+  {
+    "revision": "6918200b31b567fb695f5ae38e3232aa",
+    "url": "/shopping-cart/static/media/Venus.6918200b.jpg"
+  },
+  {
+    "revision": "bfdd84e15d343508857a0790c69a0134",
+    "url": "/shopping-cart/static/media/galaxy.bfdd84e1.jpg"
+  },
+  {
+    "revision": "66759fd08cac7a4ba46acab94c679377",
+    "url": "/shopping-cart/static/media/mercury.66759fd0.jpg"
+  },
+  {
+    "revision": "ff4d7864da805911366bd288e9610966",
+    "url": "/shopping-cart/static/media/sun.ff4d7864.jpg"
+  }
+]);
