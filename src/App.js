36c4e2d9b4b89420441303089c4d44371cad1494
Tomo5524 <ttoommoosrst@gmail.com>
2020-10-11 16:59:43 +0900

    added git host page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <div className="container">
         <NavBar />
         <Switch>
-          <Route exact path="/shopping-cart" component={Home} />
+          <Route path="/shopping-cart" component={Home} />
           <Route path="/shop" component={Shop} />
           <Route path="/cart" component={Cart} />
           <Route path="/about" component={About} />
--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
+// import React from "react";
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import Home from "./components/Home";
+// import Profile from "./components/Profile";
 
-const Routes = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/profile" component={Profile} />
-      </Switch>
-    </BrowserRouter>
-  );
-};
+// const Routes = () => {
+//   return (
+//     <BrowserRouter>
+//       <Switch>
+//         <Route path="/" component={Home} />
+//         <Route path="/profile" component={Profile} />
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// };
 
-export default Routes;
+// export default Routes;
